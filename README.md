07b52c0897d2ec1910e7973732b415602c28e415
Jozsef Geng <jozsef.geng@solution-pioneers.com>
2023-06-07 20:49:49 +0200

    initial structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# Magento Checkout Login Step extension
+
+## 1. How to install Magento Order Feedback
+
+Install via composer (recommend)
+
+```
+composer require solution-pioneers/magento-checkout-login-step
+php bin/magento setup:upgrade
+php bin/magento setup:static-content:deploy
+```
---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solution Pioneers
+ *
+ * @category    SolutionPioneers
+ * @package     SolutionPioneers_CheckoutLoginStep
+ * @copyright   Copyright (c) Solution Pioneers (https://www.solution-pioneers.com/)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'SolutionPioneers_CheckoutLoginStep',
+    __DIR__
+);
