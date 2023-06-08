108c6f66005b5681230368cd91ec3a0ab5a8caec
Jozsef Geng <jozsef.geng@solution-pioneers.com>
2023-06-08 12:59:15 +0200

    add basic login form

--- view/frontend/web/js/view/checkout-login-step.js
+++ view/frontend/web/js/view/checkout-login-step.js
@@ -4,7 +4,8 @@ define(
         'uiComponent',
         'underscore',
         'Magento_Checkout/js/model/step-navigator',
-        'Magento_Customer/js/model/customer'
+        'Magento_Customer/js/model/customer',
+        'mage/translate'
     ],
     function (
         ko,
@@ -22,12 +23,9 @@ define(
                 template: 'SolutionPioneers_CheckoutLoginStep/checkout-login-step'
             },
 
-            //add here your logic to display step,
             isVisible: ko.observable(true),
             isLogedIn: customer.isLoggedIn(),
-            //step code will be used as step content id in the component template
             stepCode: 'login',
-            //step title value
             stepTitle: 'Login',
 
             /**
@@ -36,34 +34,18 @@ define(
             */
             initialize: function () {
                 this._super();
-                // register your step
                 stepNavigator.registerStep(
                     this.stepCode,
-                    //step alias
                     null,
                     this.stepTitle,
-                    //observable property with logic when display step or hide step
                     this.isVisible,
-
                     _.bind(this.navigate, this),
-
-                    /**
-                    * sort order value
-                    * 'sort order value' < 10: step displays before shipping step;
-                    * 10 < 'sort order value' < 20 : step displays between shipping and payment step
-                    * 'sort order value' > 20 : step displays after payment step
-                    */
                     5
                 );
 
                 return this;
             },
 
-            /**
-            * The navigate() method is responsible for navigation between checkout step
-            * during checkout. You can add custom logic, for example some conditions
-            * for switching to your custom step
-            */
             navigate: function () {
 
             },
