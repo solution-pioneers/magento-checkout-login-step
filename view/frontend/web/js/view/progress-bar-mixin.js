555cca2b21250ba5af3cb61c161c136553fd1234
Jozsef Geng <jozsef.geng@solution-pioneers.com>
2023-09-25 07:12:32 +0200

    🐛 fix: resolve mouse scroll error

--- view/frontend/web/js/view/progress-bar-mixin.js
+++ view/frontend/web/js/view/progress-bar-mixin.js
@@ -1,32 +1,33 @@
 define([
-    'jquery',
-    'underscore',
-    'ko',
-    'uiComponent',
     'Magento_Customer/js/model/customer',
-    'Magento_Checkout/js/model/step-navigator',
-    'Magento_Checkout/js/view/billing-address'
-], function ($, _, ko, Component, customer, stepNavigator, billingAddress) {
+], function (customer) {
     'use strict';
+        var mixin = {
+            /**
+             * @override
+             */
+            initialize: function() {
+                this._super();
+                
+                window.scrollTo({top: 0});
+            },
 
-        return function (Component) {
-            return Component.extend({
+            /**
+             * @override
+             * 
+             * @param {Object} step 
+             */
+            navigateTo: function (step) {
+                if (customer.isLoggedIn() && step.code === 'login') {
+                    return;
+                }
 
-                /**
-                 * @override
-                 */
-                navigateTo: function (step) {
-                    if (customer.isLoggedIn() && step.code === 'login') {
-                        return;
-                    }
-    
-                    if (step.code === 'shipping') {
-                        billingAddress().needCancelBillingAddressChanges();
-                    }
-                    stepNavigator.navigateTo(step.code);
-                },
-            });
-        }
+                this._super();
+            }
+       };
 
+       return function (target) {
+           return target.extend(mixin);
+       };
    }
 );
